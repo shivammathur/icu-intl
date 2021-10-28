0e94a86228135d7a4a2fa28e5d448796fedeaf2c
Shivam Mathur <shivam_jpr@hotmail.com>
2021-10-28 20:36:54 +0530

    Use source URL from release tags

--- src/build-icu4c.sh
+++ src/build-icu4c.sh
@@ -1,7 +1,7 @@
-curl -o /tmp/icu.tgz -sL https://github.com/unicode-org/icu/releases/download/release-"${ICU/./-}"/icu4c-"${ICU/./_}"-src.tgz
+curl -o /tmp/icu.tgz -sL https://github.com/unicode-org/icu/archive/refs/tags/release-"${ICU/./-}".tar.gz
 tar -zxf /tmp/icu.tgz -C /tmp
 (
-  cd /tmp/icu/source || exit 1
+  cd /tmp/icu-release-"${ICU/./-}"/icu4c/source || exit 1
   sudo mkdir /usr/local/icu
   sudo ln -sf /usr/include/locale.h /usr/include/xlocale.h || true
   ./configure --prefix=/usr/local/icu
