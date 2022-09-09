b8952c89be4da90658f17286e64a1d3ad6a32ad6
Shivam Mathur <shivam_jpr@hotmail.com>
2022-09-09 08:13:33 +0000

    Fix gh setup in install-requirements.sh

--- src/install-requirements.sh
+++ src/install-requirements.sh
@@ -5,9 +5,13 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y automake curl git gnupg make pkg-config software-properties-common sudo zstd
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-for ppa in ppa:git-core/ppa ppa:ondrej/php https://cli.github.com/packages; do
+curl -o /usr/share/keyrings/gh.gpg -sL https://cli.github.com/packages/githubcli-archive-keyring.gpg
+chmod go+r /usr/share/keyrings/gh.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/gh.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/gh.list
+for ppa in ppa:git-core/ppa ppa:ondrej/php; do
   LC_ALL=C.UTF-8 apt-add-repository "$ppa" -y
 done
+apt-get update
 apt-get install -y git gh
 if ! check_package php"$VERSION"; then
   curl -sSLO https://github.com/shivammathur/php-builder/releases/latest/download/install.sh
