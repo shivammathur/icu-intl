2356f1b7aff45832a467fa7ac38be2d1d8c51339
Shivam Mathur <shivam_jpr@hotmail.com>
2025-11-20 07:26:04 +0000

    Fix downloading ICU source

--- src/build-icu4c.sh
+++ src/build-icu4c.sh
@@ -1,7 +1,8 @@
-curl -o /tmp/icu.tgz -sL https://github.com/unicode-org/icu/archive/refs/tags/release-"${ICU/./-}".tar.gz
+(curl -fL "https://github.com/unicode-org/icu/archive/refs/tags/release-${ICU/./-}.tar.gz" -o /tmp/icu.tgz && gzip -t /tmp/icu.tgz 2>/dev/null) || \
+(curl -fL "https://github.com/unicode-org/icu/archive/refs/tags/release-${ICU}.tar.gz" -o /tmp/icu.tgz && gzip -t /tmp/icu.tgz 2>/dev/null)
 tar -zxf /tmp/icu.tgz -C /tmp
 (
-  cd /tmp/icu-release-"${ICU/./-}"/icu4c/source || exit 1
+  cd /tmp/icu-release-"${ICU/./-}"/icu4c/source 2>/dev/null || cd /tmp/icu-release-"$ICU"/icu4c/source || exit 1
   sudo mkdir /usr/local/icu
   sudo ln -sf /usr/include/locale.h /usr/include/xlocale.h || true
   ./configure --prefix=/usr/local/icu
